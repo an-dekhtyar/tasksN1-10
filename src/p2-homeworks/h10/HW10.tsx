bb7e24d2424c3ff17b05373a69fe48bcd2f6132d
antdkhtr <antd3149@gmail.com>
2021-06-09 19:28:23 +0300

    11 task

--- src/p2-homeworks/h10/HW10.tsx
+++ src/p2-homeworks/h10/HW10.tsx
@@ -3,7 +3,9 @@ import { useDispatch, useSelector } from "react-redux";
 import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
 import { loading } from "./bll/loadingReducer";
 import { AppStoreType } from "./bll/store";
-import style from "../../p1-main/m1-ui/u1-app/App.module.css";
+import loader from './loader.gif'
+import titleStyle from "../../p1-main/m1-ui/u1-app/App.module.css"
+import style from './HW10.module.css'
 
 function HW10() {
     const dispatch = useDispatch()
@@ -20,12 +22,12 @@ function HW10() {
     return (
         <div>
             <hr/>
-            <span className={style.hw}>#HW10</span>
+            <span className={titleStyle.hw}>#HW10</span>
 
             {/*should work (должно работать)*/}
             {loadingProcess
                 ? (
-                    <div>крутилка...</div>
+                    <div className={style.loader}><img src={loader}/></div>
                 ) : (
                     <div>
                         <SuperButton onClick={setLoading}>set loading...</SuperButton>
--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,30 +1,42 @@
 import React, {useState} from "react";
 import SuperRange from "./common/c7-SuperRange/SuperRange";
 import SuperDoubleRange from "./common/c8-SuperDoubleRange/SuperDoubleRange";
+import Slider, { Range } from 'rc-slider';
+import 'rc-slider/assets/index.css';
+import style from './HW11.module.css'
+import titleStyle from "../../p1-main/m1-ui/u1-app/App.module.css"
 
 function HW11() {
     const [value1, setValue1] = useState(0);
     const [value2, setValue2] = useState(100);
+    const [value,rangeSetValue] = useState([value1, value2])
+    const rangeOnchangeHandler = [setValue1, setValue2]
+    const onChange = (state:number[]) => {
+        setValue1(state[0])
+        setValue2(state[1])
+        rangeSetValue([...state])
+    }
 
     return (
-        <div>
-            <hr/>
-            homeworks 11
+        <>
+            <span className={titleStyle.hw}>#HW11</span>
+        <div className={style.hw11block}>
+
 
             {/*should work (должно работать)*/}
-            <div>
-                <span>{value1}</span>
-                <SuperRange
-                    // сделать так чтоб value1 изменялось
-                />
+            <div className={style.slider}>
+                <div className={style.value}>{value1}</div>
+                <div className={style.inputWidth}>
+                <Slider className={style.inputWidth} value={value1} onChange={setValue1}/>
+                </div>
             </div>
 
-            <div>
-                <span>{value1}</span>
-                <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
-                />
-                <span>{value2}</span>
+            <div className={style.range}>
+                <div className={style.value}>{value1}</div>
+                <div className={style.inputWidth}>
+                    <Range defaultValue={value} onChange={onChange} />
+                </div>
+                <div className={style.value}>{value2}</div>
             </div>
 
             <hr/>
@@ -33,7 +45,8 @@ function HW11() {
             {/*<AlternativeSuperDoubleRange/>*/}
             <hr/>
         </div>
-    );
+        </>
+            );
 }
 
 export default HW11;
